ae59b4bd6c1767d7ef25b036d1e2acd83bc7a2ac
Vaishnavi Yadav <105958722+Vaishxx@users.noreply.github.com>
2025-06-21 17:41:06 +0530

    Update 01_hello.py

--- 01_hello.py
+++ 01_hello.py
@@ -2,11 +2,7 @@
    and triple appostrophy is used when the comment is made in multiple line'''
 
 
-print('''raat din jise manga tha duaon me
- dekho gaur se khi me vhi to nhi
- mw vo rng hu jo chdke kbhi chuthe na daman se
- tumhein pyar se pyar hone lgega 
- mere sth shamein bita kr to dekho''') # this is also a comment.this is used for single line comment.
+print('''Hello is this a para''') # this is also a comment.this is used for single line comment.
  # in the above line triple approstrophy is used to print because it is of multiple line.
 
 # shotrcut key to comment line/lines is to first select then click ctrl+/
